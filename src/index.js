ce860e1c67c7e00aaf3d921952031a10dbb5af4a
Lorena Zuazo <pepa_zz@hotmail.com>
2020-12-01 10:33:58 -0300

    tarea 1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css'
+import CounterApp from './CounterApp';
+
+
+
+const divRoot = document.querySelector('#root');
+ReactDOM.render(<CounterApp value={50} />, divRoot) 
+ 
+
